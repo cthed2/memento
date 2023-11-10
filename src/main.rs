82b475be82c3f21b13c53128289419dfbe9510f0
cthed2 <cristhian.ipanaque@gmail.com>
2023-11-09 23:37:06 -0500

    corrige version 2.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
-use regex::Regex; 
+use regex::Regex;
 use ansi_term::Colour;
 use unidecode::unidecode;
 
@@ -13,21 +13,21 @@ fn main() -> io::Result<()> {
     }
 
     let file_path = &args[1];
-    let keywords = &args[2..];
+    let keywords = &args[2..].iter().map(|k| unidecode(k).to_lowercase()).collect::<Vec<String>>();
     let keywords_regex = build_keywords_regex(keywords).map_err(|e| {
         eprintln!("Error building regex: {}", e);
         std::io::Error::new(std::io::ErrorKind::Other, "Regex build error")
     })?;
 
-    let file = File::open(file_path).map_err(|error| {
-        eprintln!("Error opening file: {}", error);
-        std::io::Error::new(std::io::ErrorKind::Other, "File open error")
-    })?;
+    let file = File::open(file_path)?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
         let line = line?;
-        print_highlighted_line(&line, &keywords_regex);
+        let normalized_line = unidecode(&line).to_lowercase();
+        if keywords.iter().all(|keyword| normalized_line.contains(keyword)) {
+            println!("{}", highlight_keywords(&line, &keywords_regex));
+        }
     }
 
     Ok(())
@@ -36,16 +36,15 @@ fn main() -> io::Result<()> {
 fn build_keywords_regex(keywords: &[String]) -> Result<Regex, regex::Error> {
     let pattern: String = keywords
         .iter()
-        .map(|k| regex::escape(&unidecode(&k.to_lowercase())))
+        .map(|k| regex::escape(k))
         .collect::<Vec<String>>()
         .join("|");
     Regex::new(&pattern)
 }
 
-fn print_highlighted_line(line: &str, keywords_regex: &Regex) {
-    let highlighted_line = keywords_regex.replace_all(line, |caps: &regex::Captures| {
+fn highlight_keywords(line: &str, keywords_regex: &Regex) -> String {
+    keywords_regex.replace_all(line, |caps: &regex::Captures| {
         Colour::Red.paint(&caps[0]).to_string()
-    });
-    println!("{}", highlighted_line);
+    }).to_string()
 }
 
